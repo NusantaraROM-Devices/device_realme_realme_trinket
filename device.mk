50b51d305195197788c597efbea72241105940f9
Erfan Abdi <erfangplus@gmail.com>
2019-09-26 19:40:20 +0330

    realme_trinket: Write Fingerprint HAL Adapter
    vendor.oppo.hardware.biometrics.fingerprint@2.1 -> android.hardware.biometrics.fingerprint@2.1
    
    Change-Id: Id33db73425bb5e92ef5c45e2f8d40fa462942af3
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -60,6 +60,9 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.realme_trinket
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
