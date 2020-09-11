3cc02b3289ff46f8864be134dad30781c725901b
Harshit Jain <god@hyper-labs.tech>
2020-09-11 21:24:21 +0530

    realme_trinket: Set target vndk version as 29 (Q) and ship all vndk29 libs
    Signed-off-by: Harshit Jain <god@hyper-labs.tech>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -14,6 +14,7 @@ $(call inherit-product-if-exists, vendor/realme/realme_trinket/realme_trinket-ve
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_EXTRA_VNDK_VERSIONS := 29
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1600
