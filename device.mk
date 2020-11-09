9d686e677589885bf6ebb015d0f23bf4221dfe7d
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-11-09 08:24:00 +0700

    realme_trinket: Import WCNSS qcom cfg from RMX1911EX_11_OTA_1590

--- device.mk
+++ device.mk
@@ -153,6 +153,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl
