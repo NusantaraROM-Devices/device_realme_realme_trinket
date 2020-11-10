7a990cd378d7dc637bccf71b2c3b59df8ad0e41f
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-11-10 12:35:13 +0700

    realme_trinket: Drop framework_compability_matrix and import manifest

--- device.mk
+++ device.mk
@@ -93,6 +93,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest.xml
+
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
