2a07b70f04d1f7c2a40e01d53f84d6dbbcf4e373
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-11-21 20:25:29 +0700

    realme_trinket: init: Update build fp and desc to coral nov

--- init/init_trinket.cpp
+++ init/init_trinket.cpp
@@ -79,7 +79,7 @@ void vendor_load_properties() {
 
 
     property_override("ro.control_privapp_permissions", "log");
-    property_override("ro.build.description", "sunfish-user 11 RP1A.201105.002 6869500 release-keys");
-    property_override("ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201105.002/6828489:user/release-keys");
-    property_override("ro.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201105.002/6828489:user/release-keys");
+    property_override("ro.build.description", "coral-user 11 RP1A.201105.002-6869500-release-keys");
+    property_override("ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys");
+    property_override("ro.build.fingerprint", "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys");
 }
